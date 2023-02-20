b65ba7d0822c6986aa1db5400aa44a0f4d19e51d
RD Codigo <rdcodigo@gmail.com>
2023-02-20 16:23:01 -0300

    add ex010 bootcamp pan java

---
+++ dio/bootcamp-pan-java/ex010 Lista Encadeada/src/NoListaEncadeada.java
@@ -0,0 +1,45 @@
+public class NoListaEncadeada<T> {
+    private T conteudo;
+    private NoListaEncadeada proximoNo = null;
+
+    public NoListaEncadeada(T conteudo){
+        this.conteudo = conteudo;
+    }
+
+    public NoListaEncadeada(T conteudo, No proximoNo) {
+        this.conteudo = conteudo;
+        this.proximoNo = proximoNo;
+    }
+
+    public T getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(T conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    public NoListaEncadeada getProximoNo() {
+        return proximoNo;
+    }
+
+    public void setProximoNo(NoListaEncadeada proximoNo) {
+        this.proximoNo = proximoNo;
+    }
+
+    @Override
+    public String toString() {
+        return "No{" + conteudo + '}';
+    }
+
+    public String toStringEncadeado() {
+        String str = "No{" + conteudo + "}";
+
+        if(proximoNo != null){
+            str += "->" + proximoNo.toString();
+        }else{
+            str += "->null";
+        }
+        return str;
+    }
+}
