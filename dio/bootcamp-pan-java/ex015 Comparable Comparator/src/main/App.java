21a240f8523c7e3fbfef6b8ce4e74371e2e31abc
RD Codigo <rdcodigo@gmail.com>
2023-02-24 20:27:42 -0300

    add ex015 bootcamp pan java

---
+++ dio/bootcamp-pan-java/ex015 Comparable Comparator/src/main/App.java
@@ -0,0 +1,50 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import componentes.ComparatorCor;
+import componentes.ComparatorIdade;
+import componentes.ComparatorNomeCorIdade;
+import componentes.Gato;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        List<Gato> meusGatos = new ArrayList<>(){{
+            add(new Gato("Jon", 12, "preto"));
+            add(new Gato("Simba", 6, "tigrado"));
+            add(new Gato("Jon", 18, "amarelo"));
+        }};
+        meusGatos.sort(Comparator.comparing(Gato::getNome));
+
+
+        System.out.println("--\tOrdem de Inserção\t---");
+        System.out.println(meusGatos);
+
+        System.out.println("--\tOrdem aleatória\t---");
+        Collections.shuffle(meusGatos);
+        System.out.println(meusGatos);
+
+        System.out.println("--\tOrdem Natural (Nome)\t---");
+        Collections.sort(meusGatos);
+        System.out.println(meusGatos);
+
+        System.out.println("--\tOrdem Idade\t---");
+//        Collections.sort(meusGatos, new ComparatorIdade());
+        meusGatos.sort(new ComparatorIdade());
+        System.out.println(meusGatos);
+
+        System.out.println("--\tOrdem cor\t---");
+//        Collections.sort(meusGatos, new ComparatorCor());
+        meusGatos.sort(new ComparatorCor());
+        System.out.println(meusGatos);
+
+        System.out.println("--\tOrdem Nome/Cor/Idade\t---");
+//        Collections.sort(meusGatos, new ComparatorNomeCorIdade());
+        meusGatos.sort(new ComparatorNomeCorIdade());
+        System.out.println(meusGatos);
+    }
+}
