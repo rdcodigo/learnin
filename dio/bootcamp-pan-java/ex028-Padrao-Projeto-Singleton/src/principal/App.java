16a187ff32e60b9a885a2c0bd14fdab9f02b0208
Diego Rodrigues <rdcodigo@gmail.com>
2023-03-10 08:57:31 -0300

    add 8 bootcamp pan java

---
+++ dio/bootcamp-pan-java/ex028-Padrao-Projeto-Singleton/src/principal/App.java
@@ -0,0 +1,26 @@
+package principal;
+
+import componentes.SingletonEager;
+import componentes.SingletonLazy;
+import componentes.SingletonLazyHolder;
+
+public class App {
+    public static void main(String[] args) {
+		// Singleton
+		
+		SingletonLazy lazy = SingletonLazy.getInstancia();
+		System.out.println(lazy);
+		lazy = SingletonLazy.getInstancia();
+		System.out.println(lazy);
+		
+		SingletonEager eager = SingletonEager.getInstancia();
+		System.out.println(eager);
+		eager = SingletonEager.getInstancia();
+		System.out.println(eager);
+		
+		SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+		System.out.println(lazyHolder);
+		lazyHolder = SingletonLazyHolder.getInstancia();
+		System.out.println(lazyHolder);
+    }
+}
